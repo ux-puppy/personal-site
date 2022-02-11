fcca4486aeb996b898e92f2104eef246256a949b
Satyam_Chandra <satyamchandra09@gmail.com>
2022-02-11 12:19:08 +0530

    Draft1

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,9 +1,9 @@
 const degrees = [
   {
-    school: 'Amity University',
-    degree: 'B.Tech. Computer Science and Engineering',
-    link: 'https://amity.edu/',
-    year: 2017,
+    school: 'Karnavati University',
+    degree: 'B.Des Bachelor of Design',
+    // link: 'https://amity.edu/',
+    year: 2020,
   },
 ];
 
